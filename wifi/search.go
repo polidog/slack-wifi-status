6fdf97f4314ad0e17b20cb0efc23ab1b3843975f
polidog <polidogs@gmail.com>
2017-10-24 11:25:40 +0900

    wifi取得失敗時にrecoverする

--- wifi/search.go
+++ wifi/search.go
@@ -1,7 +1,18 @@
 package wifi
 
-import "github.com/yelinaung/wifi-name"
+import (
+	"github.com/yelinaung/wifi-name"
+	"log"
+)
 
 func GetWifiName() string {
+
+	defer func() {
+		err := recover()
+		if err != nil {
+			log.Println("wifi error")
+			log.Println(err)
+		}
+	}()
 	return wifiname.WifiName()
 }
